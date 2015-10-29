996a6155fb2e43d7e6111bbebe68c809877e691e
Timothy <tpg_phone@yahoo.com>
2015-10-29 15:55:05 -0500

    Setup ApiControllers

---
+++ EmptyMvc/Api/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmptyMvc.Api
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/products")]
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>();
+
+            products.Add(new Product { Id = 1, Name = "iPhone 4S" });
+            products.Add(new Product { Id = 1, Name = "iPhone 5" });
+            products.Add(new Product { Id = 1, Name = "iPhone 5S" });
+
+            return products;
+        }
+    }
+
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
